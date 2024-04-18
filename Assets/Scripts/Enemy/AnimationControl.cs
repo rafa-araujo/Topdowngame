c9221f0ca61e06e69dcfc3e714bdc749122398a0
rafa-araujo <rafacarlosaraujo@gmail.com>
2024-04-18 19:58:03 -0300

    Combate: movimentacao

--- Assets/Scripts/Enemy/AnimationControl.cs
+++ Assets/Scripts/Enemy/AnimationControl.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class AnimationControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator anim;
+
+    private void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayAnim(int value)
     {
-        
+        anim.SetInteger("transition", value);
     }
 }
