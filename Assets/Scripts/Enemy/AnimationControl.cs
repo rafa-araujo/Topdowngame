937f23927709693f2b6645931e611ab9fdf025cf
rafa-araujo <rafacarlosaraujo@gmail.com>
2024-04-19 16:43:19 -0300

    Combate: ataque inimigo

--- Assets/Scripts/Enemy/AnimationControl.cs
+++ Assets/Scripts/Enemy/AnimationControl.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class AnimationControl : MonoBehaviour
 {
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float radius;
+    [SerializeField] private LayerMask playerLayer;
+
+
+
     private Animator anim;
 
     private void Start()
@@ -14,5 +20,25 @@ public class AnimationControl : MonoBehaviour
     public void PlayAnim(int value)
     {
         anim.SetInteger("transition", value);
+    }
+
+    public void Attack()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(attackPoint.position, radius, playerLayer);
+
+        if(hit != null)
+        {
+            //detecta colisão com player
+            Debug.Log("bateu no player");
+        }
+        else
+        {
+
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, radius);
     }
 }
